0dec925104dacb485d5771a7b11458e68464e5ae
EsbenLaursen <Esben.laursen@gmail.com>
2016-11-20 14:20:11 +0100

    Some entities added

---
+++ BookingApplicationSolution/BookingApp/Entities/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace BookingApp
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Persons { get; set; }
+        public string Description { get; set; }
+
+    }
+}
