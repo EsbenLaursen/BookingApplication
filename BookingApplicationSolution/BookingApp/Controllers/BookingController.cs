43fd784c548aecc5de7283b15dfb252e17454202
agmam <ag-mam@live.dk>
2016-11-30 11:37:04 +0100

    BookingCheckout view and controller added

---
+++ BookingApplicationSolution/BookingApp/Controllers/BookingController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingApp.Controllers
+{
+    public class BookingController : Controller
+    {
+        // GET: Booking
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Post: booking
+        
+        public ActionResult BookingCheckout()
+        {
+            return View();
+        }
+    }
+}
