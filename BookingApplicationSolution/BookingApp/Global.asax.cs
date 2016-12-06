2e162ed7b827513339ac68c3a5cfc904223e6f43
Emildall <emildall.ed@gmail.com>
2016-12-06 23:58:56 +0100

    image gallery updated added different files for multiple language packets

--- BookingApplicationSolution/BookingApp/Global.asax.cs
+++ BookingApplicationSolution/BookingApp/Global.asax.cs
@@ -20,5 +20,21 @@ namespace BookingApp
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             
         }
+
+        public void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            // Code that runs on application startup                                                            
+            HttpCookie cookie = HttpContext.Current.Request.Cookies["CultureInfo"];
+            if (cookie != null && cookie.Value != null)
+            {
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(cookie.Value);
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(cookie.Value);
+            }
+            else
+            {
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("English");
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("English");
+            }
+        }
     }
 }
