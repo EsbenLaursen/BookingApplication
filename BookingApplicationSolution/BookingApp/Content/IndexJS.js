0ade73a0e506bb51a2f5ac86fc20ba9b924c7e2b
Emildall <emildall.ed@gmail.com>
2016-12-09 10:46:14 +0100

    shit commit..

---
+++ BookingApplicationSolution/BookingApp/Content/IndexJS.js
@@ -0,0 +1,38 @@
+﻿    (function() {
+        var $lightbox = $("<div class='lightbox'></div>");
+        var $img = $("<img>");
+        var $caption = $("<p class='caption'></p>");
+
+        // Add image and caption to lightbox
+
+        $lightbox
+            .append($img)
+            .append($caption);
+
+        // Add lighbox to document
+
+        $('body').append($lightbox);
+
+        $('.lightbox-gallery img')
+            .click(function(e) {
+                e.preventDefault();
+
+                // Get image link and description
+                var src = $(this).attr("src");
+                var cap = $(this).attr("alt");
+
+                // Add data to lighbox
+
+                $img.attr('src', src);
+                $caption.text(cap);
+
+                // Show lightbox
+
+                $lightbox.fadeIn('fast');
+
+                $lightbox.click(function() {
+                    $lightbox.fadeOut('fast');
+                });
+            });
+
+    }());
