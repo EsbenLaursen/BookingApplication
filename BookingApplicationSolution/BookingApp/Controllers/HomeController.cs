87c79f82df773e81747f352617228b77e6d28b3d
Emildall <emildall.ed@gmail.com>
2016-12-19 20:25:40 +0100

    nada

--- BookingApplicationSolution/BookingApp/Controllers/HomeController.cs
+++ BookingApplicationSolution/BookingApp/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
-﻿    using DLL.Entities;
+﻿using DLL.Entities;
 using DLL.Gateways;
 using DLL.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
 using System.Web.Mvc;
 using DLL;
 
@@ -14,7 +12,6 @@ namespace BookingApp.Controllers
     {
         private EmailGateway eg = new DllFacade().GetEmailGateway();
         private IServiceGateway<Image> im = new DllFacade().GetImageGateway();
-        IServiceGateway<Booking> bg = new DllFacade().GetBookingGateway();
         private IServiceGateway<FootCare> fc = new DllFacade().GetSFootCareGateway();
         private IServiceGateway<Room> rm = new DllFacade().GetRoomGateway();
         private ReviewGateway rg = new DllFacade().GetReviewGateway();
@@ -74,7 +71,9 @@ namespace BookingApp.Controllers
 
         [HttpPost]
         public ActionResult SendEmail(string email, string name, string message)
-        { 
+        {
+            
+           
             if (email != null || name != null || message != null)
             { EmailFormModel efm = new EmailFormModel();
                 efm.FromName = name;
@@ -106,11 +105,6 @@ namespace BookingApp.Controllers
 
             }
             return RedirectToAction("Review");
-        }
-
-        public ActionResult GoToAdminView()
-        {
-            return Redirect("http://localhost:6263");
         }
     }
 }
